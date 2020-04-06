a0504d6965764cccdc967553151bcf50dbc686ac
ECecillo <ECecillo>
2020-04-06 22:42:11 +0200

    ajout du logo à la page

--- home.php
+++ home.php
@@ -47,6 +47,8 @@ if (isset($_POST["valider"])) {
     integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
   </script>
 
+  <!-- Logo -->
+  <link rel="icon" href="./img/pinter.png" type="image/icon type"> 
   <!-- Mon style -->
   <style>
     @import url('style.css');
