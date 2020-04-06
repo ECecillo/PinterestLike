79866af01eed328b40598f9d51fd2f45dccb992f
ECecillo <ECecillo>
2020-04-06 22:29:44 +0200

    ajout de tout le style pour la page home.php

--- home.php
+++ home.php
@@ -34,45 +34,73 @@ if (isset($_POST["valider"])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <!-- Css Bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <!-- Js Bootstrap -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  </script>
+
   <!-- Mon style -->
   <style>
     @import url('style.css');
   </style>
+  <!-- Mon script -->
+  <link rel="script" href="./script.js">
 </head>
 
 <body>
 
-                                                            <!-- Partie sur la navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+  <!-- Partie sur la navbar -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+  <header>
+    <div class="menu-toggle" id="hamburger">
+      <i class="fas fa-bars"></i>
     </div>
-  </nav>
-                                                      <!-- Partie sur les images  -->
+    <div class="overlay"></div>
+    <div class="container">
+      <nav>
+        <h1 class="brand"><a href="home.php">Pin<span>ter</span>est</a></h1>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Add Pictures</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <!-- Partie sur les images  -->
+
+  <div class="category_paragraph">
+    <p>Which pictures do you wanna show ? </p> <br>
+  </div>
+  <div class="category_selector">
+    <div class="btn-group dropright">
+      <button class="btn btn-light btn-lg" type="button">
+        Select a Category
+      </button>
+      <button type="button" class="btn btn-lg btn-dark dropdown-toggle dropdown-toggle-split"
+        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="sr-only">Toggle Dropdown</span>
+      </button>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Nature</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Animals</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Life</a>
+      </div>
+    </div>
+  </div>
 
   <h1><strong>Toutes les photos</strong></h1>
   <!-- Affichage des jeux  -->
@@ -120,7 +148,54 @@ if (isset($_POST["valider"])) {
         </a>
 
       </div>
+      <div class="card card-tall" style="background-image:var(--bg-I7);">
+        <a class="" href="">
+          <span></span>
+        </a>
 
+      </div>
+      <div class="card card-tall" style="background-image:var(--bg-I8);">
+        <a class="" href="">
+          <span></span>
+        </a>
+
+      </div>
+      <div class="card card-tall" style="background-image:var(--bg-I9);">
+        <a class="" href="">
+          <span></span>
+        </a>
+
+      </div>
+      <div class="card card-tall" style="background-image:var(--bg-I10);">
+        <a class="" href="">
+          <span></span>
+        </a>
+
+      </div>
+      <div class="card card-tall" style="background-image:var(--bg-I11);">
+        <a class="" href="">
+          <span></span>
+        </a>
+
+      </div>
+      <div class="card card-tall" style="background-image:var(--bg-I12);">
+        <a class="" href="">
+          <span></span>
+        </a>
+
+      </div>
+      <div class="card card-tall" style="background-image:var(--bg-I13);">
+        <a class="" href="">
+          <span></span>
+        </a>
+
+      </div>
+      <div class="card card-tall" style="background-image:var(--bg-I14);">
+        <a class="" href="">
+          <span></span>
+        </a>
+
+      </div>
     </div>
   </div>
 </body>
