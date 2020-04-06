6f51896bf23b8455ec4d50ae872a7856642b7189
CECILLON ENZO p1805901 <enzo.cecillon@etu.univ-lyon1.fr>
2020-04-06 16:54:28 +0200

    modif de l'empcement du root

--- home.php
+++ home.php
@@ -46,5 +46,167 @@ echo $stateMsg;
 </head>
 <body>
     
+      <!-- Affichage des jeux  -->
+      <div>
+        <div class="photo-grid" style="margin: 1rem 1rem;">
+          <!-- 1er message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image: var(--bg-I1);" alt="Image d'un jeu de mémoire du nom de Memoria">
+            <!-- <a class="" href="">
+              <span></span>
+            </a> -->
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  <b> Memoria </b>
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 2ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I2);" alt="Image d'un jeu de mémoire type Window">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  2
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 3ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I3);">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  3
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 4ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I4)" alt="Une autre Image d'un jeu Window">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  4
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 5ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I5);">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  5
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 6ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I6);">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  6
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 7ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I7);">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  7
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 8ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I8);">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  8
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 9ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I9);">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  9
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 10ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I10);">
+            <a class="" href="script.js">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  10
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 11ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I11);">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  11
+                </h6>
+              </header>
+            </div>
+          </div>
+          <!-- 12ieme message ----------------------------------------------------------------- -->
+          <div class="card card-tall" style="background-image:var(--bg-I12);">
+            <a class="" href="">
+              <span></span>
+            </a>
+            <div class="module">
+              <header>
+                <h6 class="title">
+                  12
+                </h6>
+              </header>
+            </div>
+          </div>
+
+        </div>
+      </div>
 </body>
 </html>
