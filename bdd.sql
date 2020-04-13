838c9b60c5ecdf037424f01af65b4e94b5f5fffb
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2020-04-13 20:28:49 +0200

    ajout des lignes pour les nouvelles images

--- bdd.sql
+++ bdd.sql
@@ -55,35 +55,20 @@ CREATE TABLE IF NOT EXISTS Photo (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-
 INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (1,"brunel-johnson-k8zVbh6Duk0-unsplash","Man in a Street",3);
+            VALUES (1,'street.jpeg',"Street",3);
 INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (2,'https://images.unsplash.com/photo-1545559054-8f4f9e855781?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',"stars",1);
+            VALUES (2,'stars.jpeg',"stars",1);
 INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (3,"david-clode-CGSFeqa3kG0-unsplash","monkey",2);
+            VALUES (3,'birds',"birds",2);
 INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (4,"charles-deluvio-AQRp2NH-O8k-unsplash","dog",2);
+            VALUES (4,'bats.jpeg',"bats",2);
 INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (5,"https://images.unsplash.com/photo-1584709521360-ff0f5bbda444?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=691&q=80","insecte2",2);
+            VALUES (5,'city.jpeg',"bats",3);
 INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (6,"https://images.unsplash.com/photo-1557008075-7f2c5efa4cfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=642&q=80","fox",2);
+            VALUES (6,'dog.jpeg',"bats",2);
 INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (7,"danny-giebe-VOG9q8Kz4XA-unsplash","Montain day",1);
-INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (8,"voy-zan-R44UWCU7pwg-unsplash","Montain",1);
-INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (9,'annie-spratt-ccjLnZC8hT4-unsplash',"stuff",3);
-INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (10,"https://images.unsplash.com/photo-1545334610-35dd6680f5bb?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80","lake",1);
-INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (11,"https://images.unsplash.com/photo-1546074340-48089b234842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80","Montain",1);
-INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (12,"https://images.unsplash.com/photo-1531145877453-9b68a77926b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80","bat",2);
-INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (13,"https://images.unsplash.com/photo-1548092352-4944c775dd75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80","snow",1);
-INSERT INTO Photo(photoId,nomFich,description,catId)
-            VALUES (14,"https://images.unsplash.com/photo-1518457607834-6e8d80c183c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80","lava",1);
+            VALUES (7,'rock.jpeg',"bats",1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
