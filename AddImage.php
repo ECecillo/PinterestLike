ae30e37788173e0217f144aa4830eaa4b43433c4
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2020-04-28 19:00:35 +0200

    fin de la mise à niv de ce que lucas à fait

--- AddImage.php
+++ AddImage.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-require_once ('./config/configuration.php');
-require_once (PATH_LIB . 'bd.php');
-require_once (PATH_LIB . 'utilisateur.php');
-require_once (PATH_LIB . 'discussion.php');
-require_once (PATH_LIB . 'add_funct.php');
+require_once('./config/configuration.php');
+require_once(PATH_LIB . 'bd.php');
+require_once(PATH_LIB . 'utilisateur.php');
+require_once(PATH_LIB . 'discussion.php');
+require_once(PATH_LIB . 'add_funct.php');
 
 
 $link = getConnection($dbHost, $dbUser, $dbPwd, $dbName);
@@ -18,21 +18,19 @@ if (isset($_POST["validate"])) {
   }
   $size_max = 904860;
   $size_Image = filesize($_FILES['Image']['tmp_name']);
-   if ($size_Image > $size_max){
-     echo "You have exceeded the allowed file size. </br>";
-   }
-   else {
-     $destination_directory=dirname(__FILE__)."/assets/img/";
-     move_uploaded_file($_FILES["Image"]["tmp_name"],$destination_directory.$_POST["Description"]."_".$_SESSION["pseudo"].".".$extension);
-     $req="INSERT INTO Photo("."nomFich,description,catId".")
-                 VALUES (" .
-                                "'" .$_POST["Description"]."_". $_SESSION["pseudo"] .".".$extension."', " .
-                                "'" . $_POST["Description"]. "', " .
-                                "'" . $_POST["Category"]. "')";
-     executeUpdate($link, $req);
-     header('Location: description.php');
-   }
-
+  if ($size_Image > $size_max) {
+    echo "You have exceeded the allowed file size. </br>";
+  } else {
+    $description = $_POST["Description"];
+    $pseud = $_SESSION["pseudo"];
+    $cate = $_POST["Category"];
+    $constName = $description . '_' . $pseud . '.' . $extension;
+    $destination_directory = dirname(__FILE__) . "/assets/img/";
+    move_uploaded_file($_FILES["Image"]["tmp_name"], $destination_directory . $constName);
+    $req = "INSERT INTO Photo(nomFich,`description`,catId) VALUES ('$constName', '$description',  '$cate')";
+    executeUpdate($link, $req);
+    header('Location: description.php');
+  }
 }
 
 ?>
@@ -49,12 +47,12 @@ if (isset($_POST["validate"])) {
     <nav>
       <h1 class="brand"><a href="home.php">Pin<span>ter</span>est</a></h1>
       <ul>
-        <li><a href="../home.php">Home</a></li>
-        <?php  if (isset($_SESSION["logged"])){
-        if ($_SESSION["logged"]=="yes") {
+        <li><a href="home.php">Home</a></li>
+        <?php if (isset($_SESSION["logged"])) {
+          if ($_SESSION["logged"] == "yes") {
             echo "<li><a href='AddImage.php'>Add image</a></li>";
           }
-        }?>
+        } ?>
         <li style="margin-top: -0.5625rem;">
           <a class="nav-link" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Category &#8659;
@@ -66,31 +64,29 @@ if (isset($_POST["validate"])) {
           </div>
         </li>
         <li>
-          <?php   if (isset($_SESSION["logged"])){
-          if ($_SESSION["logged"]=="yes") {
-           echo "<form action='home.php' method='post'>
+          <?php if (isset($_SESSION["logged"])) {
+            if ($_SESSION["logged"] == "yes") {
+              echo "<form action='home.php' method='post'>
            <li><a><input type='submit' name='logout' value='logout' style='border:none;background:none;' /></a></li>
          </form>";
-         }
-         else {
-           echo "<li><a href='login.php'>Login</a></li>";
-         }
-       }
-         else {
-           echo "<li><a href='login.php'>Login</a></li>";
-         }?>
+            } else {
+              echo "<li><a href='login.php'>Login</a></li>";
+            }
+          } else {
+            echo "<li><a href='login.php'>Login</a></li>";
+          } ?>
 
         </li>
       </ul>
     </nav>
   </div>
-  <?php if (isset($_SESSION["logged"])){
-  if ($_SESSION["logged"]=="yes") {
-    echo "<h1><strong>Welcome ".$_SESSION['pseudo']." <br/></strong></h1>";
-    echo AffDate($_SESSION["date"]);
+  <?php if (isset($_SESSION["logged"])) {
+    if ($_SESSION["logged"] == "yes") {
+      echo "<h1><strong>Welcome " . $_SESSION['pseudo'] . " <br/></strong></h1>";
+      echo AffDate($_SESSION["date"]);
+    }
   }
-}
-   ?>
+  ?>
   <!-- à compléter -->
   <h1 style="text-align: center; color:red">Add Image</h1>
   <form enctype="multipart/form-data" action="AddImage.php" style="border:2px solid #ccc; border-radius: 30px;" method="POST">
@@ -98,14 +94,14 @@ if (isset($_POST["validate"])) {
       <div class="fillform" style="margin: 1rem;">
         <label for="image"><b>Add image:</b></label>
         <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
-        <input type="file"  name="Image"  value="parcourir" required>
+        <input type="file" name="Image" value="parcourir" required>
         <br>
         <br>
         <label for=""><b>Categorie: </b></label>
-        <select id="Image"name="Category" required>
+        <select id="Image" name="Category" required>
           <option value=""> select a Category </option>
-          <?php echo fill_category($link);?>
-          </select>
+          <?php echo fill_category($link); ?>
+        </select>
         <br>
         <br>
         <label for="image"><b> Description: </b></label>
